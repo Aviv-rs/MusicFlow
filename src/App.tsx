69c6cd60ee84d891ba10d2226e7c1a11479a3628
Aviv R <aviv80010@gmail.com>
2026-04-17 16:35:27 +0300

    feat: added loader component for pending routes for better ux

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,20 @@ import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 import { ThemeProvider } from "./components/ThemeProvider";
 import { Toaster } from "./components/ui/sonner";
+import { Spinner } from "./components/ui/spinner";
+
+function DefaultPending() {
+  return (
+    <div className="flex flex-1 items-center justify-center h-screen w-screen">
+      <Spinner className="size-8" />
+    </div>
+  );
+}
 
 const router = createRouter({
   routeTree,
   defaultPreload: "viewport",
+  defaultPendingComponent: DefaultPending,
 });
 
 declare module "@tanstack/react-router" {
